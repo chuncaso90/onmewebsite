ab433f0f380eba9c24b0c23b2fda26e00518efb1
chuncaso90 <72043079+chuncaso90@users.noreply.github.com>
2021-12-03 16:01:03 -0800

    added build

---
+++ build/precache-manifest.ddebc58d3b4ac771ad8292719af486b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a061984e1b63887caccd601bd9b9b43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e08bbc3abf73885c525",
+    "url": "/static/css/main.b9e771b0.chunk.css"
+  },
+  {
+    "revision": "7b75d1472d47e1d12aa0",
+    "url": "/static/js/2.0e37905e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.0e37905e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e08bbc3abf73885c525",
+    "url": "/static/js/main.a2738be3.chunk.js"
+  },
+  {
+    "revision": "2dced7b885a329c3eb4c",
+    "url": "/static/js/runtime-main.d1ad1431.js"
+  }
+]);
